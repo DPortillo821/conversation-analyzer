45df08753f013191b42ba446ea6a10ffe6912991
Daniel Portillo <daniel_portillo@utexas.edu>
2018-07-08 14:58:00 -0500

    Cleanup main.py

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from conversation_analyzer import ConversationAnalyzer
 def main():
     # TODO: validate number of args. Must be exactly one. Must be a txt file.
     conversation_file = open(argv[1], 'r')
+    
     conversation_analyzer = ConversationAnalyzer(conversation_file)
     conversation_analyzer.parse_conversation()
+
     prompt = conversation_analyzer
+    prompt.intro = 'Starting...\nType "help" to get a list of possible queries to execute\nType "quit" to quit the program'
+    prompt.ruler = '-'
     prompt.prompt = '>>> '
-    prompt.cmdloop("Starting...\nType 'help' to get a list of possible queries to execute\nType 'quit' to quit the program ")
+    prompt.cmdloop()
 
 if __name__ == '__main__':
    main()
